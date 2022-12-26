baae6ff6cfcc09c8ad2420ad7412b6bf6cfc7d75
shrushti mane <shrustimanexyz@gmail.com>
2022-12-26 09:25:48 +0530

    minor change

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const connection = require('../connection')
 const {insertQuery,updateQuery} = require("../sqlquerys");
+const jwt_decode = require('jwt-decode');
 var router = express.Router();
 
 
@@ -61,15 +62,17 @@ router.get('/:postId', async(req, res) => {
 router.get('/byuserId/:userId', async(req, res) => {
     const { userId } = req.params;
     var output = {};
-    const querySql = 'SELECT p.*,tlike.totalLike,tComent.totalComments,u.name FROM posts p  LEFT OUTER JOIN users u ON p.userId=u.id   LEFT JOIN (   select COUNT(pl.id)as totalLike ,pl.postId from posts_likes pl    where pl.isLike = 1 AND pl.is_active = 1 GROUP BY pl.postId  ) tlike on tlike.postId = p.id    LEFT JOIN (   select COUNT(pc.id)as totalComments ,pc.postId from posts_comments pc    where pc.is_active = 1 GROUP BY pc.postId  ) tComent on tComent.postId = p.id  WHERE p.userId = "'+userId+'"  order by  p.id desc';
+    const querySql = 'SELECT p.*,tlike.totalLike,tComent.totalComments,u.name, pll.isLike FROM posts p  LEFT OUTER JOIN users u ON p.userId=u.id LEFT OUTER JOIN posts_likes pll ON pll.likeUserId = "'+userId+'" and p.id = pll.postId   LEFT JOIN (   select COUNT(pl.id)as totalLike ,pl.postId from posts_likes pl    where pl.isLike = 1 AND pl.is_active = 1 GROUP BY pl.postId  ) tlike on tlike.postId = p.id    LEFT JOIN (   select COUNT(pc.id)as totalComments ,pc.postId from posts_comments pc    where pc.is_active = 1 GROUP BY pc.postId  ) tComent on tComent.postId = p.id  WHERE p.userId = "'+userId+'"  order by  p.id desc';
     const rows = await connection({ querys: querySql, values: [] });
     // console.log("jjjjjj---------",rows)
     res.send(rows)    
  });
  router.post('/searchpost', async(req, res) => {
+     var {id} = jwt_decode(req.token);
     const { searchfield } = req.body;
     var output = {};
-    const querySql = "SELECT p.*,tlike.totalLike,tComent.totalComments,u.name FROM posts p LEFT OUTER JOIN users u ON p.userId=u.id   LEFT JOIN (   select COUNT(pl.id)as totalLike ,pl.postId from posts_likes pl    where pl.isLike = 1 AND pl.is_active = 1 GROUP BY pl.postId  ) tlike on tlike.postId = p.id    LEFT JOIN (   select COUNT(pc.id)as totalComments ,pc.postId from posts_comments pc    where pc.is_active = 1 GROUP BY pc.postId  ) tComent on tComent.postId = p.id  WHERE p.postTitle LIKE '%"+searchfield+"%' OR p.description LIKE '%"+searchfield+"%' order by  p.id desc";
+    var search = searchfield ? searchfield : ""
+    const querySql = "SELECT p.*,tlike.totalLike,tComent.totalComments,u.name, pll.isLike FROM posts p LEFT OUTER JOIN users u ON p.userId=u.id  LEFT OUTER JOIN posts_likes pll ON pll.likeUserId = '"+id+"' and p.id = pll.postId LEFT JOIN (   select COUNT(pl.id)as totalLike ,pl.postId from posts_likes pl    where pl.isLike = 1 AND pl.is_active = 1 GROUP BY pl.postId  ) tlike on tlike.postId = p.id    LEFT JOIN (   select COUNT(pc.id)as totalComments ,pc.postId from posts_comments pc    where pc.is_active = 1 GROUP BY pc.postId  ) tComent on tComent.postId = p.id  WHERE p.postTitle LIKE '%"+search+"%' OR p.description LIKE '%"+search+"%' order by  p.id desc";
     const rows = await connection({ querys: querySql, values: [] });
     // console.log("jjjjjj---------",rows)
     res.send(rows)    
@@ -120,6 +123,13 @@ router.get('/byuserId/:userId', async(req, res) => {
                        }
     }
    
+ });
+  router.get('/comments/:postId', async(req, res) => {
+    const today = new Date();
+    const { postId } = req.params;
+   const querySql = 'SELECT c.*,u.name,u.photo FROM posts_comments c LEFT OUTER JOIN users u ON c.commentUserId = u.id WHERE c.postId = '"+postId+"'  order by  c.id desc';
+    const rows = await connection({ querys: querySql, values: [] });
+     res.send(rows)    
  });
 //export this router to use in our index.js
 module.exports = router;
